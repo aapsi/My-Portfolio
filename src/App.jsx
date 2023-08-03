61ba4e723ece996ee4e3683f34ab9a0402bc6e0f
aapsi <aapsi0001@gmail.com>
2023-08-04 03:43:15 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import { BrowserRouter } from "react-router-dom"
+import { About, Contact, Experience, Feedbacks, 
+Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+
 const App = () => {
   return (
-    <div>My Porfolio</div>
+    // browserROuter -> will allow us to route
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-cover bg-no-repeat bg-center">
+          <div>
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles'
+import { navLinks } from '../constants/constants'
+import {  menu, close } from '../assets'
+import logo from '/ak-logo.png'
 
 const Navbar = () => {
+  const [active, setActive] = useState('');
+
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5
+    top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link 
+          to="/"
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            // points to where we are on the page
+            window.scrollTo(0, 0)
+            // this will scroll to the top of the page
+          }}>
+            <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+            <p className='text-white text-[18px] font-bold cursor-pointer'>Adrian
+              <span className='sm:block hidden'>
+                Khaira
+              </span>
+            </p>
+          </Link>
+      </div>
+    </nav>
   )
 }
 
