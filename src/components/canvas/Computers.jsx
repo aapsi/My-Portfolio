b790e216979fa552eaf86ca4c2af2bc04d605b5f
aapsi <aapsi0001@gmail.com>
2023-08-08 23:39:37 +0530

    comp size update

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -24,7 +24,7 @@ const Computers = ({ isMobile }) => {
         <ambientLight intensity={0.5} />
         <primitive
           object={computer.scene}
-          scale={isMobile ? 0.25 : 0.60}
+          scale={isMobile ? 0.40 : 0.60}
           position={isMobile ? [0, -3, -2.2] : [0, -4.0, -1.5]}
           rotation={[-0.01, -0.2, -0.1]}
         />
